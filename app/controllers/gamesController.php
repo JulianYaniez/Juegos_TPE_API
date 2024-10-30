eb797c1d3c63cb940f6eac586323b9e0bff4bf86
Julian Yañez <julianfidel06@gmail.com>
2024-10-30 20:31:25 -0300

    agregado getAll, getById y POST (filtro a mitad)

--- app/controllers/gamesController.php
+++ app/controllers/gamesController.php
@@ -12,5 +12,55 @@ class gamesController {
         $this->view = new JSONView();
     }
 
-    
+    public function getGames($req, $res){
+
+        if(isset($req->params->id)){
+            $id = $req->params->id;
+
+            $game = $this->model->getGameById($id);
+
+            if(!$game){
+                return $this->view->response("El juego con el id= $id no existe", 404);
+            }
+            return $this->view->response($game, 200);
+        }
+        $games = $this->model->getGames();
+        return $this->view->response($games, 200);
+    }
+    public function updateGame($req, $res){
+        $id = $req->params->id;
+        $game = $this->model->getGameById($id);
+
+        if(!$game){
+            return $this->view->response("El juego con el id= $id no existe", 404);
+        }
+        if(!isset($req->body->title) || !isset($req->body->genre) || !isset($req->body->genre) || !isset($req->body->genre) || !isset($req->body->genre)){
+            return $this->view->response('falta completar campos', 400);
+        }
+
+        $title = $req->body->title;
+        $genre = $req->body->genre;
+        $distributor = $req->body->distributor;
+        $price = $req->body->price;
+        $date = $req->body->date;
+
+        $this->model->updateGame($id, $title, $genre, $distributor, $price, $date);
+
+        $game = $this->model->getGameById($id);
+        $this->view->response($game, 200);
+    }
+    public function filter($req, $res){
+        $price = $req->query->precio;
+        if(!$price){
+            return $this->view->response('no hay ningun valor asignado', 400);
+        }
+
+        $games = $this->model->filter($price);
+
+        if(!$games){
+            return $this->view->response('No existen juegos con el filtro seleccionado', 404);
+        }
+
+        $this->view->response($games, 200);
+    }
 }
--- app/models/gamesModel.php
+++ app/models/gamesModel.php
@@ -9,5 +9,29 @@ class gamesModel extends model {
         parent::__construct();
     }
 
-    
+    public function getGames(){
+
+        $query = $this->db->prepare("SELECT * FROM juegos");
+        $query->execute();
+
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function getGameById($id){
+
+        $query = $this->db->prepare("SELECT * FROM juegos WHERE id=?");
+        $query->execute([$id]);
+
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+    public function updateGame($id, $title, $genre, $distributor, $price, $date){
+        $query = $this->db->prepare("UPDATE juegos SET id=?,titulo=?,genero=?,id_distribuidora=?,precio=?,fecha_salida=? WHERE 1");
+        $query->execute([$id, $title, $genre, $distributor, $price, $date]);
+
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+    public function filter($price){
+        
+        $query = $this->db->prepare("SELECT * FROM juegos WHERE precio > ?");
+        $query->execute([$price]);
+    }
 }
